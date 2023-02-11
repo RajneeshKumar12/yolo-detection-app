71dc6b5b35e01911321dc9a9f8cc3e775b2f16e7
EC2 Default User <ec2-user@ip-172-31-61-234.ec2.internal>
2023-02-11 07:04:49 +0000

    Added code to server

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3
+
+RUN apt-get update
+RUN apt-get install -y python3-pip
+
+RUN apt install -y libgl1-mesa-glx
+
+RUN apt-get install python3 -y
+
+RUN pip install --upgrade pip
+COPY requirements.txt ./
+
+RUN pip3 install -r requirements.txt
+
+COPY . ./
+
+CMD ["python3", "./app.py"]
+
--- yolo_detection_images.py
+++ yolo_detection_images.py
@@ -42,7 +42,7 @@ def get_predection(image,net,LABELS,COLORS):
 
     # determine only the *output* layer names that we need from YOLO
     ln = net.getLayerNames()
-    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
 
     # construct a blob from the input image and then perform a forward
     # pass of the YOLO object detector, giving us our bounding boxes and
