6040079b5f9da94e651cc8e7c3f8f6f4e3c76247
RajneeshKumar12 <43611534+RajneeshKumar12@users.noreply.github.com>
2023-03-04 22:47:35 +0530

    Update index.js

--- static/index.js
+++ static/index.js
@@ -7,7 +7,7 @@ window.onload = () => {
 			let formData = new FormData();
 			formData.append('image' , input.files[0]);
 			$.ajax({
-				url: "http://54.152.133.29/:5000/detectObject", // fix this to your liking
+				url: "http://localhost:5000/detectObject", // fix this to your liking
 				type:"POST",
 				data: formData,
 				cache: false,
